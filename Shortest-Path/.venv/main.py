30968b4698e06d2317030ce8751f6f733b9271d1
CashmereKey <dhruvsethi870@gmail.com>
2023-07-10 16:15:25 +0530

    API code

---
+++ Shortest-Path/.venv/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+import subprocess
+
+app = Flask(__name__)
+
+@app.route("/test/<int:i>/<int:j>")
+def test(i,j):
+    # Compile the C++ code
+    subprocess.run(["g++", "test.cpp", "-o", "test.exe"])
+    
+    # Execute the compiled C++ code and capture the output
+    result = subprocess.run(["./test.exe"], input=str(i)+" "+str(j), capture_output=True, text=True)
+
+    # Return the output as a response
+    return jsonify({"output": result.stdout})
+
+@app.route("/astar")
+def astar():
+    subprocess.run(["g++","test3.cpp","-o", "test3.exe", "-lsqlite3"])
+
+    result = subprocess.run(["./test3.exe"], capture_output=True, text=True)
+
+    return jsonify({"output": result.stdout})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Shortest-Path/.venv/test.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i, j;
+    cin >> i >> j;
+    cout << "Sum of element: " << i + j;
+    // cout << "Hello world..." << endl;
+    return 0;
+}
